8307f05983db350ce3da10ef2ea63d94218dcdd9
Mads Ronnow <roennowmads@gmail.com>
2017-08-18 18:13:57 +0200

    Initial Commit

---
+++ Assets/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject burgerObject;
+    private float m_currentTime = 0;
+    public float m_spawnDelay = 1;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        m_currentTime += Time.deltaTime;
+        if (m_currentTime > m_spawnDelay)
+        {
+            m_currentTime = 0;
+            Instantiate(burgerObject, new Vector3(0.0f, 6.0f,0.0f), Quaternion.identity);
+        }
+
+
+	}
+}
